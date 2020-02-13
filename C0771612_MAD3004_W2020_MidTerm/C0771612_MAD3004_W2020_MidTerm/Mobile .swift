dd9a53af255d42f19f47f8b7fe736fbf17cc5b1b
shreemarella <mvsrikanth96@gmail.com>
2020-02-13 18:44:02 -0500

    [Shree Marella] mobile class  created along with its  attributes and inherited from bill  class

---
+++ C0771612_MAD3004_W2020_MidTerm/C0771612_MAD3004_W2020_MidTerm/Mobile .swift
@@ -0,0 +1,39 @@
+//
+//  Mobile .swift
+//  C0771612_MAD3004_W2020_MidTerm
+//
+//  Created by Shree Marella on 2020-02-13.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
+
+class Mobile : Bill
+{
+    var mobileManufacturerName : String
+    var planName               : String
+    var mobileNumber           : Int
+    var internetGbUsed         : String
+    var minutesUsed            : String
+    
+    init(_ billIdentificationNumber:Int,_ billDate:Date,_ billType:String,_  totalBillAmount:Double,_ mobileManufacturerName:String,_ planName:String,_ mobileNumber:Int,_ internetGbUsed:String,_ minutesUsed:String)
+    {
+        self.mobileManufacturerName = mobileManufacturerName
+        self.planName               = planName
+        self.mobileNumber           = mobileNumber
+        self.internetGbUsed         = internetGbUsed
+        self.minutesUsed            = minutesUsed
+        super.init(billIdentificationNumber,billDate,billType,totalBillAmount)
+    }
+    override func Display()
+    {
+        super.Display()
+        print("mobileManufacturerName  : \(self.mobileManufacturerName)")
+        print("planName                : \(self.planName)")
+        print("mobileNumber            : \(self.mobileNumber)")
+        print("internetGbUsed          : \(self.internetGbUsed)")
+        print("minutesUsed             : \(self.minutesUsed)")
+        
+    }
+}
+
