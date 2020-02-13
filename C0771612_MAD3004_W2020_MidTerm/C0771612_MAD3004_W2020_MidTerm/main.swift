131fa4351e7b86465942b792c39a5063bb2d2ebc
shreemarella <mvsrikanth96@gmail.com>
2020-02-13 18:09:04 -0500

    [Shree Marella] created Protocol IDisplay

---
+++ C0771612_MAD3004_W2020_MidTerm/C0771612_MAD3004_W2020_MidTerm/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  C0771612_MAD3004_W2020_MidTerm
+//
+//  Created by Shree Marella on 2020-02-13.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
+
