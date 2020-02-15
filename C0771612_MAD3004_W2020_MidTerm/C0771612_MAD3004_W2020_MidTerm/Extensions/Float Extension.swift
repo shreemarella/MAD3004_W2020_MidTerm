c10dc7540a6d1ef73686ae92501024160a10a9b0
shreemarella <mvsrikanth96@gmail.com>
2020-02-14 20:05:45 -0500

    [Shree Marella] created  2 new files for int and float extensions

---
+++ C0771612_MAD3004_W2020_MidTerm/C0771612_MAD3004_W2020_MidTerm/Extensions/Float Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Float Extension.swift
+//  C0771612_MAD3004_W2020_MidTerm
+//
+//  Created by Shree Marella on 2020-02-14.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
